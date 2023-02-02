58f660a72a237b60f124070f6b2176181d8d23df
Krystal Kimmel <kabkimmel@gmail.com>
2023-02-01 18:59:14 -0600

    solved problem 03

--- problems/03-arrow-rest-sum.js
+++ problems/03-arrow-rest-sum.js
@@ -10,6 +10,19 @@ arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 45
 arrowRestSum(0); // => 0
 ***********************************************************************/
 //
+const arrowRestSum = (...nums)  => {
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    let el = nums[i];
+    sum += el;
+  }
+  return sum;
+}
+
+
+console.log(arrowRestSum(3,5,6)); // => 14
+console.log(arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9)); // => 45
+console.log(arrowRestSum(0)); // => 0
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
